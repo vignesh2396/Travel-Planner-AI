f9d9f36d1620984f5cea8db6ceab68de61e0996b
vignesh2396 <vigneshvky2396@gmail.com>
2025-10-03 10:43:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     PROVIDER = os.environ.get("PROVIDER", "gemini").lower()
 
-    GEMINI_API_KEY = "AIzaSyB5ER3FZDpfZ0CDaDbyifTHCF70IM0C-Js"
+    GEMINI_API_KEY = "your_Gemini_API_key"
     GEMINI_MODEL = "gemini-2.5-flash"
 
     # OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
